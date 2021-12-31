add7e132a0d03ab155295a240deea1f641f07c6a
brandadr <brandadrian@gmail.com>
2021-12-31 08:56:01 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿var fileToRead = "fileToRead.txt";
+
+//Old  way of handling async stuff
+var lines = File.ReadAllLinesAsync(fileToRead).ContinueWith(result =>
+{
+    if (result.IsFaulted)
+    {
+        Console.Error.WriteLine("An error occured...");
+        return;
+    }
+
+    foreach (var line in result.Result)
+    {
+        Console.WriteLine(line);
+    }
+});
+
+//Better way of handling async stuff
+await ReadFileAsnc(fileToRead);
+
+async Task ReadFileAsnc(string fileName)
+{
+    var lines = await File.ReadAllLinesAsync(fileName);
+    foreach (var line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
